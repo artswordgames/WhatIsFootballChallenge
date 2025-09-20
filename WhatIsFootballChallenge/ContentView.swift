f89e288cfd919ecff406d1f6498b3a907c4a8eb9
Andrew Straight <devArtsw0rd7@protonmail.com>
2025-09-20 00:59:29 -0500

    Initial Commit

---
+++ WhatIsFootballChallenge/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WhatIsFootballChallenge
+//
+//  Created by artsw0rd on 9/20/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WhatIsFootballChallenge/WhatIsFootballChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatIsFootballChallengeApp.swift
+//  WhatIsFootballChallenge
+//
+//  Created by artsw0rd on 9/20/25.
+//
+
+import SwiftUI
+
+@main
+struct WhatIsFootballChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
